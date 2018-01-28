907b6202949fb15387debcce654ebded774cc75c
j <j.walder@lancaster.ac.uk>
2018-01-28 20:04:38 +0100

    Initial skelton package structure included.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='dlutils',
+      version='0.1',
+      description='simple set of helpers for DL ',
+      url='https://github.com/snafus/dlutils',
+      #author='',
+      #author_email='flyingcircus@example.com',
+      #license='MIT',
+      packages=['dlutils'],
+      install_requires = [
+             'theano',
+             'numpy',
+             'keras',
+             'sklearn',
+             'bcolz'
+            ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
+
+
---
+++ tests/test_dlutils.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+import dlutils
+
+class TestStart(TestCase):
+    def test_silly(self):
+        self.assertTrue(1==1)
+
